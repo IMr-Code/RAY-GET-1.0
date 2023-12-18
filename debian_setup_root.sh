be661261da555c7bab55cc300ff4e59b7ce84a07
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:33:40 +0100

    Create debian_setup_root.sh

---
+++ debian_setup_root.sh
@@ -0,0 +1,18 @@
+apt update
+apt install git -y
+apt install python3 -y
+apt install python3-mechanize -y
+apt install python3-requests -y
+apt install python3-bs4 -y
+git clone https://github.com/CalebMarcelino/RAY-GET-1.0.git
+chmod 777 ray-get.sh
+sleep 2
+clear
+echo ""
+echo ""
+echo ""
+echo "Todas as dependencias forão instaladas corretamente, não precisa rodar esse script novamente"
+echo "Rode o comando o seguinte comando para iniciar a ferramenta "
+echo ""
+echo "bash ray-get.sh"
+
