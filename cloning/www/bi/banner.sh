4eb4904248184a17a3e021180fabe47f295c73ac
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:02:24 +0100

    Add files via upload

---
+++ cloning/www/bi/banner.sh
@@ -0,0 +1,12 @@
+clear
+echo -e "\e[35m"
+echo "====================================================="
+echo -e "\e[0m"
+echo -e "\e[1;31m"
+echo -e "RAY-GET  1.0" | figlet
+echo -e "\e[0m"
+echo -e "\e[35m"
+echo "====================================================="
+echo -e "\e[0m"
+curl 'https://rayget-developer.netlify.app/'
+echo ""
