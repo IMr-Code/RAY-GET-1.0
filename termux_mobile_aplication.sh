55a5108d0f38e13ee173d5ef1728da8591b19ab4
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:36:00 +0100

    Create  termux_mobile_aplication.sh

---
+++ termux_mobile_aplication.sh
@@ -0,0 +1,18 @@
+apt update
+pkg install git -y
+pkg install python3 -y
+pkg install python3-mechanize -y
+pkg install python3-requests -y
+pkg install python3-bs4 -y
+git clone https://github.com/CalebMarcelino/RAY-GET-1.0.git
+chmod 777 ray-get.sh
+sleep 2
+clear
+echo ""
+echo ""
+echo ""
+echo "Todas as dependencias forão instaladas corretamente, não precisa rodar esse script novamente"
+echo "Rode o comando o seguinte comando para iniciar a ferramenta "
+echo ""
+echo "bash ray-get.sh"
+
