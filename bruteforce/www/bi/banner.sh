007d8a7a8908c439b2b41519cb2676ddacb9b559
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:07:24 +0100

    Add files via upload

---
+++ bruteforce/www/bi/banner.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#
+#Desenvolvedor: Caleb Marcelino (Mr Code)
+#Nenhum Sistema eh 100% seguro
+#
+clear
+echo -e "\e[35m"
+echo "====================================================="
+echo -e "\e[0m"
+echo -e "\e[1;31m"
+echo -e "RAY-GET  1.0" | figlet
+echo -e "\e[0m"
+echo -e "\e[35m"
+echo "====================================================="
+echo -e "\e[0m"
+curl 'https://rayget-developer.netlify.app/'
+echo ""
---
+++ bruteforce/www/bi/banner2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#Developer: Caleb Marcelino (Mr Code)
+#
+#Treine todos os dias, eh assim que voce ficara mas forte.
+#
+echo -e "\e[35m"
+echo "====================================================="
+echo -e "\e[0m"
+echo -e "\e[1;31m"
+echo -e "RAY-GET  1.0" | figlet
+echo -e "\e[0m"
+echo -e "\e[35m"
+echo "====================================================="
+echo -e "\e[0m"
+echo "========: Developer: Caleb Marcelino (Mr Code)"
