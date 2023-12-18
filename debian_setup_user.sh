f88eaa19ac727a820afef6f1a92aced73bd58cee
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:42:13 +0100

    Update and rename debain_setup_user.sh to debian_setup_user.sh

--- debian_setup_user.sh
+++ debian_setup_user.sh
@@ -4,7 +4,6 @@ sudo apt install python3 -y
 sudo apt install python3-mechanize -y
 sudo apt install python3-requests -y
 sudo apt install python3-bs4 -y
-git clone https://github.com/CalebMarcelino/RAY-GET-1.0.git
 chmod 777 ray-get.sh
 sleep 2
 clear
