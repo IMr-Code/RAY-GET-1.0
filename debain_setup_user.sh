a5a0bd4900418c6c77218179cbd811cdb9570c6c
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:31:00 +0100

    Create debain_setup_user.sh

---
+++ debain_setup_user.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install git -y
+sudo apt install python3 -y
+sudo apt install python3-mechanize
+sudo apt install python3-requests
+sudo apt install python3-bs4
+git clone https://github.com/CalebMarcelino/RAY-GET-1.0.git
+chmod 777 ./RAY-GET-1.0/ray-get.sh
+sleep 2
+clear
+echo ""
+echo ""
+echo ""
+echo "Todas as dependencias forão instaladas corretamente, não precisa rodar esse script novamente"
+echo "Rode o comando o seguinte comando para iniciar a ferramenta "
+echo ""
+echo "bash ray-get.sh"
+
