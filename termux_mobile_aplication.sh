596f2ccf686315978a62954f150d0fc56d95860b
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-12-18 08:43:19 +0100

    Update termux_mobile_aplication.sh

--- termux_mobile_aplication.sh
+++ termux_mobile_aplication.sh
@@ -4,7 +4,6 @@ pkg install python3 -y
 pkg install python3-mechanize -y
 pkg install python3-requests -y
 pkg install python3-bs4 -y
-git clone https://github.com/CalebMarcelino/RAY-GET-1.0.git
 chmod 777 ray-get.sh
 sleep 2
 clear
